51c05c5b369b10889cf953d38c2ab2c1c44966b7
Billy <fx4038@gmail.com>
2024-04-25 13:33:44 -0400

    fix: adding comments

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get("/", (req, res) => {
 
 // Iniciar el servidor en el puerto 3000
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`App running at http://localhost:3000`);
+    console.log(`App running at ${process.env.PORT}`);
 });
